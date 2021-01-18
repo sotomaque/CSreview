4dcc4d06b0fb42a98a28faa64a5357a70c190e72
Enrique <sotomaque@gmail.com>
2021-01-17 16:09:10 -0800

    added reverse int

---
+++ leetCode/7-ReverseInteger.js
@@ -0,0 +1,15 @@
+/*
+Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+*/
+
+function reverseInteger(x) {
+  const limit = 2147483648;
+  const sign = x < 0 ? -1 : 1;
+  const n = Number(String(Math.abs(x)).split('').reverse().join(''));
+  return n > limit ? 0 : n * k;
+}
+
+console.log(reverseInteger(1534236469));
