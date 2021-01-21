c6ec3209c6629edc227c48e20e8cd7ddb3f9569f
Enrique <esotomay@starbucks.com>
2021-01-21 11:53:13 -0800

    added daily byte question

---
+++ js/interviewQuestions/vaccumCleanerRoute.js
@@ -0,0 +1,73 @@
+/*
+  Given strings representing the sequence of moves a robot makes,
+  return whether or not it will return to its original position.
+
+  The string will only contain 'L', 'R', 'U', 'D' chars.
+
+  i.e. 
+    input = 'LR'
+    output = true
+
+  i.e. 
+    input = 'URURD'
+    output = false
+*/
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  // functions
+  push(item) {
+    // push element into the items
+    this.items.push(item);
+  }
+  pop() {
+    // return top most element in the stack
+    // and removes it from the stack
+    // Underflow if stack is empty
+    if (this.items.length == 0) return 'Underflow';
+    return this.items.pop();
+  }
+  peek() {
+    // return the top most element from the stack
+    // but does'nt delete it.
+    return this.items[this.items.length - 1];
+  }
+
+  // isEmpty function
+  isEmpty() {
+    // return true if stack is empty
+    return this.items.length == 0;
+  }
+
+  // printStack function
+  printStack() {
+    var str = '';
+    for (var i = 0; i < this.items.length; i++) str += this.items[i] + ' ';
+    return str;
+  }
+}
+
+function willReturnHome(moves) {
+  if (moves.length <= 1) return false
+
+  const myStack = new Stack();
+  myStack.push(moves[0]);
+
+  for (let i = 1; i < moves.length; i++) {
+    const lastMove = myStack.peek();
+    const currentMove = moves[i];
+    // case where we are 'undoing' previous move
+    if (lastMove === 'L' && currentMove === 'R' || lastMove === 'R' && currentMove === 'L' || lastMove === 'U' && currentMove === 'D' || lastMove === 'D' && currentMove === 'U') {
+      myStack.pop();
+    } else {
+      myStack.push(currentMove)
+    }
+  }
+
+  return myStack.isEmpty();
+}
+
+console.log(willReturnHome('RUULLDRD'))
