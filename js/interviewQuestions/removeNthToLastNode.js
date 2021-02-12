9370d6231ff2262016eb97a4e263d00ecf9f9f3c
Enrique <sotomaque@gmail.com>
2021-02-12 09:58:41 -0800

    added google interview question

---
+++ js/interviewQuestions/removeNthToLastNode.js
@@ -0,0 +1,11 @@
+/*
+  This question is asked by Facebook. Given a linked list and a value n, remove the nth to last node and return the resulting list.
+
+  Ex: Given the following linked lists...
+
+  1->2->3->null, n = 1, return 1->2->null
+  1->2->3->null, n = 2, return 1->3->null
+  1->2->3->null, n = 3, return 2->3->null
+*/
+
+function removeNthToLastNode(head, n) {}
